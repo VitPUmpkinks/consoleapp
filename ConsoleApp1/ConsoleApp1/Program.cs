74a18c22e9d43be6656b7c398fb1de8a2fe033b3
Vitaly <vzhusel2@mercalis.com>
2025-07-04 10:03:59 +0300

    changes again

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World1!");
 Console.WriteLine("Hello, World2!");
 Console.WriteLine("Hello, World3!");
 Console.WriteLine("Hello, World4!");
+Console.WriteLine("Hello, World5!");
+
